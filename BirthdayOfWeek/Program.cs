12de44a208ad81dfabb4545de6a37f672b816da7
Julia Aasmaa <my_e_10@hotmail.com>
2020-10-28 16:27:10 +0200

    commit

---
+++ BirthdayOfWeek/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BirthdayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the application asks the users date of birth
+            //and displays the day of the week the user was born on
+            Console.WriteLine("Enter your year of birth: ");
+            int year = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your month of birth: ");
+            int month = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your day of birth: ");
+            int day = Int32.Parse(Console.ReadLine());
+
+            GetTheWeekDay(year, month, day);
+
+        }
+
+        public static void GetTheWeekDay(int year, int month, int date)
+        {
+            DateTime birthdate = new DateTime(year, month, date);
+            var dayOfTheWeek = birthdate.DayOfWeek;
+            Console.WriteLine($"You were born on {dayOfTheWeek}");
+        }
+    }
+}
---
+++ DateAndTime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+            Console.WriteLine($"Today: {today}");
+            int yearToday = today.Year;
+            Console.WriteLine($"Current year from today {yearToday}");
+            var weekDayToday = today.DayOfWeek;
+            Console.WriteLine($"It's {weekDayToday}!");
+            Console.WriteLine("----------------");
+
+            DateTime todayDetails = DateTime.Now;
+            Console.WriteLine($"Today in details: {todayDetails}");
+            DateTime utc = DateTime.UtcNow;
+            Console.WriteLine($"UTC Time {utc}");
+
+            //DateTime(year, month, day, hour, minute)
+            DateTime christmasEve = new DateTime(2020, 12, 24);
+            var christmasDayOfWeek = christmasEve.DayOfWeek;
+            Console.WriteLine($"This year Christmas Eve is on {christmasDayOfWeek}");
+
+
+        }
+    }
+}
